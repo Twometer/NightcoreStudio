ec6a8c0c2678d885f5d8921a53956ab89a32e978
Twometer <twometer@outlook.de>
2019-10-19 00:23:51 +0200

    Adjusted params

--- AutoNightcore/Program.cs
+++ AutoNightcore/Program.cs
@@ -15,17 +15,23 @@ namespace AutoNightcore
         [Argument(0, Description = "Audio file or URL")]
         public string AudioFile { get; }
 
-        [Argument(1, Description = "Lyrics file or URL")]
-        public string LyricsFile { get; }
-
-        [Argument(2, Description = "Wallpaper file or URL")]
+        [Argument(1, Description = "Wallpaper file or URL")]
         public string WallpaperFile { get; }
 
-        [Option(Description = "Lyrics font family", ShortName = "ff")]
+        [Argument(2, Description = "MP4 output file")]
+        public string OutputFile { get; }
+
+        [Option(Description = "Lyrics file or URL", ShortName = "l")]
+        public string LyricsFile { get; }
+
+        [Option(Description = "Lyrics font family", ShortName = "f")]
         public string FontFamily { get; }
 
-        [Option(Description = "Generate intro", ShortName = "I")]
+        [Option(Description = "Generate intro", ShortName = "i")]
         public bool GenerateIntro { get; }
+
+        [Option(Description = "Frames per second", ShortName ="r")]
+        public int FPS { get; }
 
         private void OnExecute()
         {
