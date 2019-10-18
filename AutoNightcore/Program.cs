710b2ec7569f652e6e12237abed6090bf6cb1bba
Twometer <twometer@outlook.de>
2019-10-19 00:40:01 +0200

    Argument and gen basics

--- AutoNightcore/Program.cs
+++ AutoNightcore/Program.cs
@@ -1,6 +1,8 @@
-﻿using McMaster.Extensions.CommandLineUtils;
+﻿using AutoNightcore.Generator;
+using McMaster.Extensions.CommandLineUtils;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,20 +24,62 @@ namespace AutoNightcore
         public string OutputFile { get; }
 
         [Option(Description = "Lyrics file or URL", ShortName = "l")]
-        public string LyricsFile { get; }
+        public string LyricsFile { get; } = null;
 
         [Option(Description = "Lyrics font family", ShortName = "f")]
-        public string FontFamily { get; }
+        public string FontFamily { get; } = "Arial Black";
 
         [Option(Description = "Generate intro", ShortName = "i")]
-        public bool GenerateIntro { get; }
+        public bool GenerateIntro { get; } = false;
 
-        [Option(Description = "Frames per second", ShortName ="r")]
-        public int FPS { get; }
+        [Option(Description = "Frames per second", ShortName = "r")]
+        public int FPS { get; } = 30;
 
         private void OnExecute()
         {
+            if (!File.Exists(AudioFile))
+            {
+                Console.WriteLine("Audio file does not exist");
+                return;
+            }
 
+            if (!File.Exists(WallpaperFile))
+            {
+                Console.WriteLine("Wallpaper file does not exist");
+                return;
+            }
+
+            if (OutputFile == null)
+            {
+                Console.WriteLine("Output file is required");
+                return;
+            }
+
+            bool generateLyrics = LyricsFile != null;
+            if (generateLyrics && !File.Exists(LyricsFile))
+            {
+                Console.WriteLine("Lyrics file does not exist");
+                return;
+            }
+
+            if (FPS <= 0 || FPS > 120)
+            {
+                Console.WriteLine("FPS outside of range 1...120 are not supported");
+                return;
+            }
+
+            Console.WriteLine("Generating Nightcore video...");
+            var options = new GeneratorOptions(new FileInfo(AudioFile), new FileInfo(WallpaperFile), new FileInfo(OutputFile), LyricsFile != null ? new FileInfo(LyricsFile) : null, FontFamily, GenerateIntro, FPS);
+            var generator = new NightcoreGenerator(options);
+
+            if (generator.Generate())
+            {
+                Console.WriteLine("Generated video successfully");
+            }
+            else
+            {
+                Console.WriteLine("Failed to generate video. Check error log for more info");
+            }
         }
     }
 }
