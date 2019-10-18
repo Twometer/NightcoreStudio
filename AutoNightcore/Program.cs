7e20d8fdcf0cf3e42bfb3c82d911951b362791fb
Twometer <twometer@outlook.de>
2019-10-18 23:44:02 +0200

    Create project

---
+++ AutoNightcore/Program.cs
@@ -0,0 +1,35 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoNightcore
+{
+    [HelpOption]
+    class Program
+    {
+        public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args.Length == 0 ? new string[] { "--help" } : args);
+
+        [Argument(0, Description = "Audio file or URL")]
+        public string AudioFile { get; }
+
+        [Argument(1, Description = "Lyrics file or URL")]
+        public string LyricsFile { get; }
+
+        [Argument(2, Description = "Wallpaper file or URL")]
+        public string WallpaperFile { get; }
+
+        [Option(Description = "Lyrics font family", ShortName = "ff")]
+        public string FontFamily { get; }
+
+        [Option(Description = "Generate intro", ShortName = "I")]
+        public bool GenerateIntro { get; }
+
+        private void OnExecute()
+        {
+
+        }
+    }
+}
