34f49c439798e4e4639858fa6c7b616ce0b0850d
Twometer <twometer@outlook.de>
2020-07-19 19:25:06 +0200

    Spectrum analyer effect...?

---
+++ NightcoreStudio/Effects/SpectrumEffect.cs
@@ -0,0 +1,30 @@
+﻿using CSCore;
+using CSCore.DSP;
+using NightcoreStudio.Effects.Base;
+using NightcoreStudio.Generator;
+using NightcoreStudio.Renderer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NightcoreStudio.Effects
+{
+    public class SpectrumEffect : IEffect
+    {
+        public EffectStage Stage => EffectStage.Main;
+
+        public void Apply(IRenderer renderer, Frame frame)
+        {
+            
+        }
+
+        public void Initialize(ISampleSource soundSource, GeneratorOptions options)
+        {
+            var fftSize = FftSize.Fft4096;
+            var fftBuffer = new float[(int)fftSize];
+
+        }
+    }
+}
